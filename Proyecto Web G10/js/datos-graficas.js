0784db60bc3449f7165a0df9df6ab3d1e9d844c5
Roberto Molina Díaz <102062995+Roxxete@users.noreply.github.com>
2023-05-04 23:43:14 +0200

    USUARIO DATOS DEFINITIVO

--- Proyecto Web G10/js/datos-graficas.js
+++ Proyecto Web G10/js/datos-graficas.js
@@ -9,12 +9,12 @@ var estadoPH = 'pulsado'
 function mostrarHumedad() {
     if(estadoHumedad =='sinpulsar') {
         estadoHumedad = 'pulsado'
-        document.getElementById('botonhumedad').style.background = '#AAAAAA'
+        document.getElementById('botonhumedad').style.background = '#FFFFFF'
         document.getElementById('GraficaHumedad').style.visibility = 'visible';
     }
     else if(estadoHumedad == 'pulsado') {
         estadoHumedad = 'sinpulsar'
-        document.getElementById('botonhumedad').style.background = '#FFFFFF'
+        document.getElementById('botonhumedad').style.background = '#AAAAAA'
         document.getElementById('GraficaHumedad').style.visibility = 'hidden';
     }
 }
@@ -22,12 +22,12 @@ function mostrarHumedad() {
 function mostrarSalinidad() {
     if(estadoSalinidad =='sinpulsar') {
         estadoSalinidad = 'pulsado'
-        document.getElementById('botonsalinidad').style.background = '#AAAAAA'
+        document.getElementById('botonsalinidad').style.background = '#FFFFFF'
         document.getElementById('GraficaSalinidad').style.visibility = 'visible';
     }
     else if(estadoSalinidad == 'pulsado') {
         estadoSalinidad = 'sinpulsar'
-        document.getElementById('botonsalinidad').style.background = '#FFFFFF'
+        document.getElementById('botonsalinidad').style.background = '#AAAAAA'
         document.getElementById('GraficaSalinidad').style.visibility = 'hidden';
     }
 }
@@ -35,12 +35,12 @@ function mostrarSalinidad() {
 function mostrarTemperatura() {
     if(estadoTemperatura =='sinpulsar') {
         estadoTemperatura = 'pulsado'
-        document.getElementById('botontemperatura').style.background = '#AAAAAA'
+        document.getElementById('botontemperatura').style.background = '#FFFFFF'
         document.getElementById('GraficaTemperatura').style.visibility = 'visible';
     }
     else if(estadoTemperatura == 'pulsado') {
         estadoTemperatura = 'sinpulsar'
-        document.getElementById('botontemperatura').style.background = '#FFFFFF'
+        document.getElementById('botontemperatura').style.background = '#AAAAAA'
         document.getElementById('GraficaTemperatura').style.visibility = 'hidden';
     }
 }
@@ -49,12 +49,12 @@ function mostrarTemperatura() {
 function mostrarLuminosidad() {
     if(estadoLuminosidad =='sinpulsar') {
         estadoLuminosidad = 'pulsado'
-        document.getElementById('botonluminosidad').style.background = '#AAAAAA'
+        document.getElementById('botonluminosidad').style.background = '#FFFFFF'
         document.getElementById('GraficaLuminosidad').style.visibility = 'visible';
     }
     else if(estadoLuminosidad == 'pulsado') {
         estadoLuminosidad = 'sinpulsar'
-        document.getElementById('botonluminosidad').style.background = '#FFFFFF'
+        document.getElementById('botonluminosidad').style.background = '#AAAAAA'
         document.getElementById('GraficaLuminosidad').style.visibility = 'hidden';
     }
 
@@ -63,12 +63,12 @@ function mostrarLuminosidad() {
 function mostrarPH(){
     if(estadoPH =='sinpulsar') {
         estadoPH = 'pulsado'
-        document.getElementById('botonph').style.background = '#AAAAAA'
+        document.getElementById('botonph').style.background = '#FFFFFF'
         document.getElementById('GraficaPH').style.visibility = 'visible';
     }
     else if(estadoPH == 'pulsado') {
         estadoPH = 'sinpulsar'
-        document.getElementById('botonph').style.background = '#FFFFFF'
+        document.getElementById('botonph').style.background = '#AAAAAA'
         document.getElementById('GraficaPH').style.visibility = 'hidden';
     }
 
